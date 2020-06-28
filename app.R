99b9169bd3bed792412a628f91378e8197bb28f5
Qi <wangqi12240205@163.com>
2020-06-28 15:05:55 -0400

    init app.R with sample data

---
+++ app.R
@@ -0,0 +1,30 @@
+# 01-kmeans-app
+library(shiny)
+setwd("/Users/wangqi/Desktop/Github/dashboard-Rshiny/")
+df <- read.csv("sample_data.csv")
+ui <- fluidPage(
+  headerPanel('Dashborad with R-shiny'),
+  selectInput('selectedK', 'K Variable', unique(df$K)),
+  sidebarPanel(
+    radioButtons('ycol', 'Y Variable', choices = c("Y", "Z"), 
+                 selected = "Y")
+    
+  ),
+  mainPanel(
+    plotOutput('plot1')
+  )
+)
+
+server <- function(input, output) {
+  
+  selectedData <- reactive({
+    df[df$K == input$selectedK, c("X", input$ycol)]
+  })
+  
+  output$plot1 <- renderPlot({
+    plot(selectedData(), main = input$ycol)
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
